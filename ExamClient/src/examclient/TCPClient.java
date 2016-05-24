061f8eb1aff8fca1eedc8e76c1ffec022c1364bc
mariusz <mariusz@MCs>
2016-05-24 22:24:35 +0200

    Klasa do obslugi komunikacji z serwerem.

---
+++ ExamClient/src/examclient/TCPClient.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examclient;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ *  Klasa dostarczająca metod do komunikacji z serwerem.
+ * 
+ *  @author mariusz
+ */
+public class TCPClient {
+    
+    private String serverIP;
+    private Integer serverPort;
+
+    public TCPClient(String srvIP, String port) {
+        this.serverIP = srvIP;
+        this.serverPort = Integer.parseInt(port);
+    }
+    
+    public String sendToServer(String text) throws IOException{
+        String sentence;   
+
+        Socket clientSocket = new Socket(serverIP, serverPort);   
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   
+ 
+        outToServer.writeBytes(text + '\n');   
+        sentence = inFromServer.readLine();   
+  
+        clientSocket.close();
+        
+        return sentence;
+    }
+}
