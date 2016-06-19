c88854125bcf9ff60397b9e7416a29497b6bd4e2
mariusz <mariusz@MCs>
2016-06-19 15:37:10 +0200

    przerzucenie obsługi wyjątków podczas łaczenia z serwerem do klasy obslugujacej polaczenie TCP.

--- ExamClient/src/examclient/TCPClient.java
+++ ExamClient/src/examclient/TCPClient.java
@@ -9,7 +9,10 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  *  Klasa dostarczająca metod do komunikacji z serwerem.
@@ -21,26 +24,42 @@ public class TCPClient {
     private final String serverIP;
     private final Integer serverPort;
 
-    public TCPClient(String srvIP, String port) {
+    /**
+     * Konstruktor.
+     * 
+     * @param srvIP String: adres IP serwera.
+     * @param port  String: port, na którym serwer pracuje.
+     */
+    public TCPClient(String srvIP, Integer port) {
         this.serverIP = srvIP;
-        this.serverPort = Integer.parseInt(port);
+        this.serverPort = port;
 
     }
     
-    public String sendToServer(String text) throws IOException{
-        String sentence;   
-
-        Socket clientSocket = new Socket(serverIP, serverPort);   
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   
- 
-        outToServer.writeBytes(text + '\n');
-        sentence = inFromServer.readLine();   
-  
-        outToServer.close();
-        inFromServer.close();
-        clientSocket.close();
+    /**
+     * Metoda do wysyłania i odbierania komunikatów do/z serwera.
+     * 
+     * @param text  String: text do wysłania.
+     * @return      String: text otrzymany od serwera.
+     */
+    public String sendToServer(String text){ 
+        String response = null;   
         
-        return sentence;
+        try(Socket clientSocket = new Socket(serverIP, serverPort)) {   
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());   
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));   
+
+            outToServer.writeBytes(text + '\n');
+            response = inFromServer.readLine();   
+
+            outToServer.close();
+            inFromServer.close();
+        }
+        catch(UnknownHostException ex){ return "Błąd: proszę podać poprawny adress serwera."; }
+        catch(ConnectException ex) { return "Błąd: nie można nawiązać połączenia z serwerem na wskazanym porcie."; }
+        catch(SocketException ex) {return "Błąd: nie można polaczyc sie ze wskazana siecia. Sprawdz podane dane.";}
+        catch(IOException ex){ ex.printStackTrace(); return "Błąd";}
+        
+        return response;
     }
 }
